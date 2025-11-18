8cce5d6b8b8133ad12deb0edf5a25bb533780fdf
zgnhauc <baominhchau2007@gmail.com>
2025-11-18 17:35:04 -0500

    Modify dropdowns for skin type and price range
    Updated skin type options and added price range dropdown.

--- frontend/main.py
+++ frontend/main.py
@@ -8,7 +8,7 @@ root.geometry("400x400")
 def show():
     myLabel = Label(root, text=clicked.get()).pack()
 
-options = [
+options1 = [
     "oily",
     "normal",
     "dry"
@@ -16,7 +16,18 @@ options = [
 clicked = StringVar()
 clicked.set("Select Skin Type")  
 
-drop = OptionMenu(root, clicked, *options)
+drop = OptionMenu(root, clicked, *options1)
+drop.pack()
+
+options2 = [
+    "<$100", 
+    "$100 - $200",
+    "$200+"
+]
+clicked = StringVar()
+clicked.set("Select Price Range")
+
+drop = OptionMenu(root, clicked, *options2)
 drop.pack()
 
 root.mainloop()
