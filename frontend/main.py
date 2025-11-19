326d71f394575b34e2c1555157a11421260a8462
zgnhauc <baominhchau2007@gmail.com>
2025-11-18 21:06:21 -0500

    modify frontend design

--- frontend/main.py
+++ frontend/main.py
@@ -4,6 +4,7 @@ from tkinter import *
 root = Tk()
 root.title("Amazon Makeup Filter Tool")
 root.geometry("400x400")
+root.config(bg = "light pink")
 
 def show():
     myLabel = Label(root, text=clicked.get()).pack()
@@ -17,7 +18,8 @@ clicked = StringVar()
 clicked.set("Select Skin Type")  
 
 drop = OptionMenu(root, clicked, *options1)
-drop.pack(padx = 30, pady=30)
+drop.config(bg = "violet", width=15)
+drop.pack(padx = 30, pady=70)
 
 options2 = [
     "<$100", 
@@ -28,6 +30,7 @@ clicked = StringVar()
 clicked.set("Select Price Range")
 
 drop = OptionMenu(root, clicked, *options2)
+drop.config(bg = "violet")
 drop.pack(padx = 30, pady=30)
 
 root.mainloop()
