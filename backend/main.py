0e911893113970c67f393d990b0ac9b7aa183f48
Dhyaani Shah <35607973+dhyaaniShah@users.noreply.github.com>
2025-11-20 15:01:21 -0500

    Update main.py
    added comments for clarity
    
    moved some variables inside function definitions

--- backend/main.py
+++ backend/main.py
@@ -5,17 +5,18 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+# intializes and allows for a new google chrome browser to be opened
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # product we are checking
 productURL = ("https://www.amazon.com/Face-Shop-Cleansing-Hydrating-Moisturizing/dp/B0DB6D3L9N/ref=sr_1_5_sspa?crid=17TE4QHZ0Q9DB&dib=eyJ2IjoiMSJ9.JnQIERFbDYWxZ1Jrg1eGdMQwCOHePJ6WAL_OZ9tll6n4Qap-r_rxP5j6kH8gXbLjBi3ApDBH3t8D6TYjhMgcFtrZhlty7ydHI8P0wElZHsd1PbJPlHbByt9hw5CGag9nFen8zFCSFPTbWDgwIaNG6J3AZEj859HG_pllSBrtgQN34DybSnZ98ImPRoepmOkax_-hBGkKyNpRBBYWjbcpjd9-Pc_I8gDh2RtvKm6zGVm2S_E8nCb1o5GwE9eebodDDbcRqi85wjocdhoZzxvDaimVHaM55GMNIoc9zg56u3M.zx9ocwwpAn9cOTJQHjwaZlyRhPtM_wlGwHIdo_YkDxE&dib_tag=se&keywords=makeup+and+skincare&qid=1763616645&sprefix=makeup+and+skincare%2Caps%2C163&sr=8-5-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1")
 
-driver.get(productURL) # goes to product URL
-driver.quit() # exits the URL to close
+driver.get(productURL) # opens webpage to product URL
+driver.quit() # exits the webpage of the URL to close
 
 # extracting product details
-productName = driver.find_element(By.ID, "productTitle").text # product name
-productDescriptionList = driver.find_element(By.CSS_SELECTOR, "a-unordered-list a-vertical a-spacing-mini") # product descriptions
+productName = driver.find_element(By.ID, "productTitle").text # product name is found using the ID
+productDescriptionList = driver.find_element(By.CSS_SELECTOR, "a-unordered-list a-vertical a-spacing-mini") # product descriptions is found using the CSS Selector
 # still need to get product price and reviews, store in variables productPrice and productReviews
 
 # get skin type
@@ -23,8 +24,9 @@ oilyKeywords = ["oily", "greasy", "shiny", "sticky", "glossy"]
 normalKeywords = ["normal", "combination", "even", "smooth"]
 dryKeywords = ["dry", "rough", "dehydrated", "flaky"]
 
-productSkinType = "None"
+
 def skinType(reviews): # tests skin type compatability, takes list of reviews as parameter
+  productSkinType = "None"
   sentiments = 0
   mentions = 0
   for review in reviews: # iterates through every review in reviews
@@ -53,10 +55,12 @@ def skinType(reviews): # tests skin type compatability, takes list of reviews as
     return productSkinType
 
 # check if product meets user specifications
-priceGood = False
-skinTypeGood = False
-allSpecifications = False
+
 def meetsRequirements(userSkinType, userBudget):
+  priceGood = False
+  skinTypeGood = False
+  allSpecifications = False
+  
   if userSkinType == productSkinType:
     skinTypeGood = True
   if userBudget >= productPrice:
@@ -87,3 +91,4 @@ if allSpecifications == True:
   print(productSkinType)
 else:
   print("Try another product")
+
