e72928c10920ab8ec06dc0086d70af8c3a1badcb
Dhyaani Shah <35607973+dhyaaniShah@users.noreply.github.com>
2025-11-29 22:33:01 -0500

    Create data.py
    Created basic framework for how data file will look
    
    Still need to add data to the data set

---
+++ backend/data.py
@@ -0,0 +1,17 @@
+oilyProducts = {
+  'names': ['name1', 'name2', 'nameN'],
+  'prices': [1.00, 2.00, 45.98],
+  'reviews': [oilyName1ReviewList, oilyName2ReviewList, oilyNameNReviewList]
+}
+
+normalProducts = {
+  'names': ['name1', 'name2', 'nameN'],
+  'prices': [1.00, 2.00, 45.98],
+  'reviews': [normalName1ReviewList, normalName2ReviewList, normalNameNReviewList]
+}
+
+dryProducts = {
+  'names': ['name1', 'name2', 'nameN'],
+  'prices': [1.00, 2.00, 45.98],
+  'reviews': [dryName1ReviewList, dryName2ReviewList, dryNameNReviewList]
+}
